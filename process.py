885ad1ca3852200c725c545fd61ca64d03ec5181
kicysh <71452166+kicysh@users.noreply.github.com>
2024-07-29 23:27:34 +0900

    ADD: predict_process

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
-import os
+from skimage import io, transform
+
 import torch
 import torchvision
 from torch.autograd import Variable
@@ -12,6 +13,7 @@ import torchvision.transforms as standard_transforms
 
 import numpy as np
 import glob
+from PIL import Image
 import os
 
 from data_loader import Rescale
@@ -152,3 +154,90 @@ def training_process(
     
     return net
 
+
+# normalize the predicted SOD probability map
+def normPRED(d):
+    ma = torch.max(d)
+    mi = torch.min(d)
+
+    dn = (d-mi)/(ma-mi)
+
+    return dn
+
+def save_output(image_name,pred,d_dir):
+
+    predict = pred
+    predict = predict.squeeze()
+    predict_np = predict.cpu().data.numpy()
+
+    im = Image.fromarray(predict_np*255).convert('RGB')
+    img_name = image_name.split(os.sep)[-1]
+    image = io.imread(image_name)
+    imo = im.resize((image.shape[1],image.shape[0]),resample=Image.BILINEAR)
+
+    pb_np = np.array(imo)
+
+    aaa = img_name.split(".")
+    bbb = aaa[0:-1]
+    imidx = bbb[0]
+    for i in range(1,len(bbb)):
+        imidx = imidx + "." + bbb[i]
+
+    imo.save(d_dir+imidx+'.png')
+
+
+def predict_process(
+    model_name,
+    
+    is_use_cuda = torch.cuda.is_available(),
+    image_dir = os.path.join(os.getcwd(), 'test_data', 'test_images'),
+    prediction_dir = os.path.join(os.getcwd(), 'test_data', model_name + '_results' + os.sep),
+    model_dir = '/path/to/saved_model.pth',
+
+    img_name_list = glob.glob(image_dir + os.sep + '*'),
+    ):
+    print(img_name_list)
+
+    # --------- dataloader ---------
+    test_salobj_dataset = SalObjDataset(img_name_list = img_name_list,
+                                        lbl_name_list = [],
+                                        transform=transforms.Compose([RescaleT(320),
+                                                                      ToTensorLab(flag=0)])
+                                        )
+    test_salobj_dataloader = DataLoader(test_salobj_dataset,
+                                        batch_size=1,
+                                        shuffle=False,
+                                        num_workers=1)
+
+    if is_use_cuda:
+        net.load_state_dict(torch.load(model_dir))
+        net.cuda()
+    else:
+        net.load_state_dict(torch.load(model_dir, map_location='cpu'))
+    net.eval()
+
+    # --------- 4. inference for each image ---------
+    for i_test, data_test in enumerate(test_salobj_dataloader):
+
+        print("inferencing:",img_name_list[i_test].split(os.sep)[-1])
+
+        inputs_test = data_test['image']
+        inputs_test = inputs_test.type(torch.FloatTensor)
+
+        if is_use_cuda:
+            inputs_test = Variable(inputs_test.cuda())
+        else:
+            inputs_test = Variable(inputs_test)
+
+        d1,d2,d3,d4,d5,d6,d7= net(inputs_test)
+
+        # normalization
+        pred = d1[:,0,:,:]
+        pred = normPRED(pred)
+
+        # save results to test_results folder
+        if not os.path.exists(prediction_dir):
+            os.makedirs(prediction_dir, exist_ok=True)
+        save_output(img_name_list[i_test],pred,prediction_dir)
+
+        del d1,d2,d3,d4,d5,d6,d7
